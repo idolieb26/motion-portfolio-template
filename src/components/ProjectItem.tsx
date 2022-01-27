249563eb8c10e1ef3913fbaf3d45904756da9460
idolieb26 <idolieb26@gmail.com>
2022-01-27 14:25:31 +0100

    Scroll management attempt WIP

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,9 +1,12 @@
-import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { Link, useNavigate } from "react-router-dom";
 import { projects } from "../../constants/projects"
 import { IProject } from "../Types"
 import { CgClose } from "react-icons/cg"
 import { FaExternalLinkAlt, FaCode } from "react-icons/fa"
+import { useScrollConstraints } from "../../utils/use-scroll-constraints";
+import { useWheelScroll } from "../../utils/use-wheel-scroll";
 
 const ProjectItem = ({ id }:{id:string}) => {
   const project = projects.find((project) => project.id === id);
@@ -14,9 +17,28 @@ const ProjectItem = ({ id }:{id:string}) => {
   }
 
   const { category, title, image, date, description, tags, source, visit }:IProject = project
+  const isSelected = true
+  const dismissDistance = 150;
+  const navigate = useNavigate();
+  const y = useMotionValue(0);
+  const cardRef = useRef(null);
+  const constraints = useScrollConstraints(cardRef, isSelected);
+  
+  function checkSwipeToDismiss() {
+    y.get() > dismissDistance && navigate("/");
+  }
 
+  const containerRef = useRef(null);
+  useWheelScroll(
+    containerRef,
+    y,
+    constraints,
+    checkSwipeToDismiss,
+    isSelected
+  );
+  
   return (
-    <>
+    <div ref={containerRef}>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -28,7 +50,14 @@ const ProjectItem = ({ id }:{id:string}) => {
         <Link to="/" />
       </motion.div>
       <div className="card-content-container open">
-        <motion.div className="card-content bg-white" layoutId={`card-container-${id}`}>
+        <motion.div 
+          ref={cardRef}
+          drag={isSelected ? "y" : false}
+          dragConstraints={constraints}
+          onDrag={checkSwipeToDismiss}
+          className="bg-white card-content" 
+          layoutId={`card-container-${id}`}
+        >
           <motion.div
             className="relative overflow-hidden shadow-md pb-[24rem] sm:pb-[30rem] bg-black"
             layoutId={`card-image-container-${id}`}
@@ -37,22 +66,22 @@ const ProjectItem = ({ id }:{id:string}) => {
           </motion.div>
           <motion.div className="absolute top-4 right-4">
             <Link to="/">
-              <CgClose className="text-white text-3xl" />
+              <CgClose className="text-3xl text-white" />
             </Link>
           </motion.div>
           <motion.div
             className=""
             layoutId={`title-container-${id}`}
           >
-            <div className="flex justify-between text-white bg-cyan-900 px-6 py-2">
+            <div className="flex justify-between px-6 py-2 text-white bg-cyan-900">
               <span className="font-semibold">{category}</span>
               <span className="font-semibold">{date}</span>
             </div>
-            <div className="font-semibold text-3xl mt-5 mx-6">
+            <div className="mx-6 mt-5 text-3xl font-semibold">
               <h2>{title}</h2>
             </div>
           </motion.div>
-          <motion.div className="my-5 mx-6" animate>
+          <motion.div className="mx-6 my-5" animate>
             {description}
           </motion.div>
           <hr />
@@ -69,14 +98,14 @@ const ProjectItem = ({ id }:{id:string}) => {
             </div>
             {(visit || source) && <div className={`grid ${visit && source ? "grid-cols-2" : "grid-cols-1"} pt-4`}>
               {visit &&
-              <a href={visit} target='_blank' className="bg-teal-500 hover:bg-teal-700 text-white font-semibold py-6 text-center">
-                <div className="flex justify-center items-center">
+              <a href={visit} target='_blank' className="py-6 font-semibold text-center text-white bg-teal-500 hover:bg-teal-700">
+                <div className="flex items-center justify-center">
                   <FaExternalLinkAlt className="mr-2" />
                   <span>Visiter</span>
                 </div>
               </a>}
-              {source && <a href={source} target='_blank' className="bg-teal-500 hover:bg-teal-700 text-white font-semibold py-6 text-center">
-                <div className="flex justify-center items-center">
+              {source && <a href={source} target='_blank' className="py-6 font-semibold text-center text-white bg-teal-500 hover:bg-teal-700">
+                <div className="flex items-center justify-center">
                   <FaCode className="mr-2 text-xl" />
                   <span>Voir le code</span>
                 </div>
@@ -85,7 +114,7 @@ const ProjectItem = ({ id }:{id:string}) => {
           </motion.div>
         </motion.div>
       </div>
-    </>
+    </div>
   );
 }
 
