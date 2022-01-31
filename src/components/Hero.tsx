7f67296309f5a48a109a3386b3b97c8ff08c8523
idolieb26 <idolieb26@gmail.com>
2022-01-31 14:57:16 +0100

    Maj Layout Mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,41 +3,53 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Mobile from "./Mobile";
 
+const HeroContent = () => (
+    <div className="">
+        <h1 className='text-4xl text-white'>
+            Bienvenue sur<br />
+            mon portfolio
+        </h1>
+        <div className='text-white'>
+            Mon objectif est de vous rendre inoubliable !
+        </div>
+        <div className="my-8">
+            <a href="https://www.google.com" className="btngrad text-white px-8 py-4 my-4 rounded-full shadow-lg">
+                Bouton
+            </a>
+        </div>
+    </div>
+)
+
+const Phone = () => (
+    <Canvas style={{touchAction: 'none'}} className="h-20">
+        <ambientLight intensity={0.5} />
+        <Suspense fallback={null}>
+            <PresentationControls
+                global={false} 
+                zoom={1}
+                rotation={[0, -Math.PI / 4, 0]}
+                polar={[0, Math.PI / 4]}
+                azimuth={[-Math.PI / 4, Math.PI / 4]}
+            >
+                <Mobile />
+                <ContactShadows frames={10} position={[0, -1.05, 0]} scale={10} blur={2} far={10} />
+            </PresentationControls>
+        </Suspense>
+    </Canvas>
+)
+
 const Hero = () => {
     return (
-        <section className='flex flex-row items-center justify-center my-10 h-screen'>
-            <div className="grid grid-cols-1 sm:grid-cols-2 h-full">
-            <div className="">
-                <h1 className='text-4xl text-white'>
-                    Bienvenue sur<br />
-                    mon portfolio
-                </h1>
-                <div className='text-white'>
-                    Mon objectif est de vous rendre inoubliable !
-                </div>
-                <div className="my-8">
-                    <a href="https://www.google.com" className="btngrad text-white px-8 py-4 my-4 rounded-full shadow-lg">
-                        Bouton
-                    </a>
-                </div>
+        <section className='flex flex-row items-center justify-center my-10 h-[75vh] sm:h-screen'>
+            <div className="hidden sm:grid grid-cols-2 h-full">
+                <HeroContent />
+                <Phone />
             </div>
-            <Canvas style={{touchAction: 'none'}}>
-                <ambientLight intensity={0.5} />
-                <Suspense fallback={null}>
-                    <PresentationControls
-                        global={false} 
-                        zoom={0.8}
-                        rotation={[0, -Math.PI / 4, 0]}
-                        polar={[0, Math.PI / 4]}
-                        azimuth={[-Math.PI / 4, Math.PI / 4]}
-                    >
-                        <Mobile />
-                        <ContactShadows frames={10} position={[0, -1.05, 0]} scale={10} blur={2} far={10} />
-                    </PresentationControls>
-                </Suspense>
-            </Canvas>
+            <div className="grid sm:hidden grid-cols-1 h-full">
+                <Phone />
+                <HeroContent />
+            </div>
             <Loader />
-            </div>
         </section>
     )
 }
