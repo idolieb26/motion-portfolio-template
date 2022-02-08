9747a9a77cc7144f8653b2a476bb1a64ceb86e90
idolieb26 <idolieb26@gmail.com>
2022-02-08 17:34:20 +0100

    WIP Data

--- src/Types.ts
+++ src/Types.ts
@@ -1,15 +1,57 @@
 export interface IProject {
-    category: string
-    title:  string;
-    description:  string;
-    image:  string;
-    tags: string[];
-    source?: string;
-    visit?:  string;
-    id: string;
-    date: string;
+    id: number;
+    attributes: IAttributes;
     isSelected?: boolean;
 }
+
+export interface IAttributes {
+    __typename:  string;
+    title:       string;
+    slug:        string;
+    cover:       Cover;
+    date:        string;
+    category:    Category;
+    techs:       Tech[];
+    description: string;
+    source:      string;
+    url:         string;
+}
+
+export interface Category {
+    __typename: string;
+    data:       CategoryData;
+}
+
+export interface CategoryData {
+    __typename: string;
+    attributes: PurpleAttributes;
+}
+
+export interface PurpleAttributes {
+    __typename: string;
+    Category:   string;
+}
+
+export interface Cover {
+    __typename: string;
+    data:       CoverData;
+}
+
+export interface CoverData {
+    __typename: string;
+    attributes: FluffyAttributes;
+}
+
+export interface FluffyAttributes {
+    __typename: string;
+    url:        string;
+}
+
+export interface Tech {
+    __typename: string;
+    tech:       string;
+}
+
 
 export interface ITimeElement {
     id:          number;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,30 +1,31 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { projects } from "../../constants/projects"
-import { IProject } from "../Types"
+import { IProject, IAttributes } from '../Types';
 import { gql, useQuery } from '@apollo/client';
 
-function Card({ id, title, category, image }:IProject) {
+function Card({ id, attributes }:IProject) {
+  const {title, slug, cover, date, category, techs, description, source, url}: IAttributes = attributes
   return (
     <motion.li whileHover={{ scale: 1.03 }} className="card h-[80vw] sm:h-[40vw] lg:h-[25vw]">
       <div className="card-content-container">
-        <motion.div className="shadow-md card-content" layoutId={`card-container-${id}`}>
+        <motion.div className="shadow-md card-content" layoutId={`card-container-${slug}`}>
           <motion.div
             className="card-image-container"
-            layoutId={`card-image-container-${id}`}
+            layoutId={`card-image-container-${slug}`}
           >
-            <img className="card-image" src={image} alt={title} />
+            <img className="card-image" src={`https://admin.aurelientrouble.com${cover.data.attributes.url}`} alt={title} />
           </motion.div>
           <motion.div
             className="absolute text-white left-6 top-4 max-w-[300px]"
-            layoutId={`title-container-${id}`}
+            layoutId={`title-container-${slug}`}
           >
             <span className="category">{category}</span>
             <h2>{title}</h2>
           </motion.div>
         </motion.div>
       </div>
-      <Link to={id} className={`card-open-link`} />
+      <Link to={slug} className={`card-open-link`} />
     </motion.li>
   );
 }
@@ -74,8 +75,8 @@ const Projects = ({ selectedId }:{selectedId:string|undefined}) => {
   return (
     <section id="projets" className="py-20 mx-10 lg:mx-20">
       <ul className="grid grid-cols-1 gap-8 pb-10 sm:grid-cols-2 lg:grid-cols-3">
-        {projects.map((project:IProject) => (
-          <Card key={project.id} {...project} isSelected={project.id === selectedId} />
+        {projs.map((project:IProject) => (
+          <Card key={project.attributes.slug} {...project} isSelected={project.attributes.slug === selectedId} />
         ))}
       </ul>
     </section>
