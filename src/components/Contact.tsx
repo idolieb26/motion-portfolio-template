2645059164951aa602547a32f483c16e78a8c15d
idolieb26 <idolieb26@gmail.com>
2022-08-03 22:27:45 +0200

    Contact section 11

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -150,7 +150,7 @@ const Contact = () => {
                                 </div>
                             }
                             <div>
-                                <button type="submit" className={`text-white ${isError && "bg-red-500"} ${isSubmitted && "bg-green-500"} ${(!isError && !isSubmitted) && "bg-blue-700"} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center`} disabled={isSending || isSubmitted}>
+                                <button type="submit" className={`text-white ${isSubmitted && "bg-green-500"} ${(!isError && !isSubmitted) && "bg-blue-700"} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center`} disabled={isSending || isSubmitted}>
                                     {isSending ? 
                                         <>
                                             <svg aria-hidden="true" role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
