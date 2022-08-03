079da454f6799402ba4134b24c70198cc1e0f100
idolieb26 <idolieb26@gmail.com>
2022-08-03 23:30:31 +0200

    Contact section 14

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -10,43 +10,52 @@ const Technologies = () => {
                 J'ai travaillé avec un certain nombre de technologies du monde du développement web.
                 Du Design, du Front-end jusqu'au Back-end.
             </div>
-            <div className='flex py-8 justify-evenly items-center'>
-                <div className="flex flex-col justify-center items-center">
+            <div className='flex flex-col space-y-12 md:flex-row md:space-y-0 py-8 justify-evenly items-center'>
+                <div className="flex flex-col w-full md:w-1/3 justify-center items-center">
                     <div className='text-3xl'>
                         <SiReact />
                     </div>
                     <div className="flex flex-col items-center pt-2">
-                        <div className='text-xl font-semibold'>
+                        <div className='text-xl font-semibold pb-2'>
                             Front-end
                         </div>
                         <div>
-                            Expérience avec ReactJS
+                            HTML, ReactJS, TypeScript
+                        </div>
+                        <div>
+                            CSS, SASS, TailwindCSS, Material UI
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col w-full md:w-1/3 justify-center items-center">
                     <div className='text-3xl'>
                         <CgDatabase />
                     </div>
                     <div className="flex flex-col items-center pt-2">
-                        <div className='text-xl font-semibold'>
+                        <div className='text-xl font-semibold pb-2'>
                             Back-End
                         </div>
                         <div>
-                            NodeJS et bases de données
+                            NodeJS, Python
+                        </div>
+                        <div>
+                            MongoDB, MySQL, Firebase
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col md:w-1/3 justify-center items-center">
                     <div className='text-3xl'>
                         <CgFigma />
                     </div>
                     <div className="flex flex-col items-center pt-2">
-                        <div className='text-xl font-semibold'>
+                        <div className='text-xl font-semibold pb-2'>
                             UI/UX
                         </div>
                         <div>
-                            Figma & Adobe XD
+                            Figma & XD
+                        </div>
+                        <div>
+                            Photoshop, Illustrator, After Effects
                         </div>
                     </div>
                 </div>
